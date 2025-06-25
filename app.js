c60da614d6e33b78c308ce125babec43405a6520
computerFisso <pc@fisso.com>
2025-06-25 19:05:47 +0200

    nothing much

---
+++ app.js
@@ -0,0 +1,16 @@
+let ticTacToe_square = [
+    ['', '', '' ],
+    ['', '', '' ],
+    ['', '', '' ]
+]
+
+function check()
+{
+    // check x
+    // check y
+    // check diagonal
+
+
+}
+
+console.log(ticTacToe_square[0])
